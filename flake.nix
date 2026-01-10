4b10400bc9e61edc804c5250f95a19f65fa53b0d
Clara Periago <clara.periagoserrano@ru.nl>
2026-01-10 10:15:37 +0100

    report almost done

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-latex = {
+      url = "github:szethh/nix-latex";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -11,17 +15,33 @@
       self,
       nixpkgs,
       flake-utils,
+      nix-latex,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        latex-inputs-base = (nix-latex.devShellFor system).buildInputs;
+        latex-inputs = latex-inputs-base ++ [
+          pkgs.biber
+          (pkgs.texlive.combine {
+            inherit (pkgs.texlive)
+              biblatex
+              csquotes
+              enumitem
+              environ
+              latexindent
+              multirow
+              scheme-medium
+              todonotes
+              xypic
+              ;
+          })
+        ];
       in
       {
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            # pkgs.python3
-          ];
+          buildInputs = with pkgs; [ ] ++ latex-inputs;
         };
       }
     );
